435e49876dc86aabfb3d065bd92f256cc4feb211
Lus2kk <leshenkov2007@gmail.com>
2026-05-14 16:14:20 -0700

    feat: add migrations for the database

---
+++ migrations/000001_subscriptions.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE subscriptions (
+    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    service_name TEXT NOT NULL,
+    price        INTEGER NOT NULL,
+    user_id      UUID NOT NULL,
+    start_date   DATE NOT NULL,
+    end_date     DATE NOT NULL 
+);
